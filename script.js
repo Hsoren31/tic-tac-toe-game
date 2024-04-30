20acd5ae0d11eef6a66b3cd485750882997b0995
Haley Sorensen <sorensenhaley@gmail.com>
2024-04-30 11:15:38 -0600

    Game works in the console without any winning logic

--- script.js
+++ script.js
@@ -1,101 +1,61 @@
-//Game Board function to make 3x3 board and print to the console.
- function GameBoard() {
-   //Make board of 3x3
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+//Display Board
+ const gameboard = (function () {
+   //Our board will be a 3x3 grid
+   const board = [];
+   const rows = 3;
+   const columns = 3;
 
-    for(let i = 0; i < rows; i++){
+   for(let i = 0; i < rows; i++){
+      //each row in the board array will be represented by another array
       board[i] = [];
-      for(let j = 0; j < columns; j++ ){
-         board[i].push(Cell());
+      //the columns in the rows will be represented by -
+      for (let j = 0; j < columns; j++){
+         board[i].push('-');
       }
-    }
-    //Place Marker function that will change the value of the cell on the board
-    const placeMarker = (row, column, player) => {
-      board[row][column].addMarker(player);
-    }
+   }
 
-    //This should pass the active state of the board to the console.
-    const printBoard = () => {
-      const boardWithCellValues = board.map((row) => 
-      row.map((cell) => cell.getValue()))
-      console.log(boardWithCellValues);
-    };
-    return {
-      placeMarker,
-      printBoard};
-};
-//function to determine the value of each cell
-function Cell(){
-   let value = '0';
+   return board;
+})();
 
-   const addMarker = (player) => {
-      value = player.marker;
-   };
-
-   const getValue = () => value;
-
-   return {
-      addMarker,
-      getValue
-   };
-}
-
- //Function to create player objects that hold their name and marker for the game
-
- function createPlayer (name, marker) {
+//create player objects that hold their name and marker for the game
+function createPlayer (name, marker) {
    return {
       name,
       marker
    }
- }
+};
 
- const player1 = createPlayer('player1', 'X');
- const player2 = createPlayer('player2', 'O');
+const player1 = createPlayer('player 1', 'X');
+const player2 = createPlayer('player 2', 'O');
 
-//Function that passes in both players, determines 
-//which players turn, and plays a single 'round' or 'turn;
- function GameController(player1, player2){
-   const board = GameBoard();
+//object to play the game
+const game = (function (){
+   const board = gameboard;
+   console.log(board);
 
-
-   //players go into an array to then easily pass back and forth turns
+   //Keep players in array to easily switch between turns
    const players = [
       player1,
       player2
-   ];
-
+   ]
+   
    let activePlayer = players[0];
-
    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ?
-      players[1] : players[0];
-   };
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+   }
 
    const getActivePlayer = () => activePlayer;
-   //Prints current state of board and trades turns
-   const printNewRound = () => {
-      board.printBoard();
 
-      console.log(`${getActivePlayer().name}'s turn.`);
-   };
-
+   console.log(activePlayer);
+   //Rounds
    const playRound = (row, column) => {
-      console.log(`Placing ${getActivePlayer().name}'s marker...`);
-
-      board.placeMarker(row, column, getActivePlayer());
-
+      board[row][column] = getActivePlayer().marker;
+      console.log(board);
       switchPlayerTurn();
-      printNewRound();
-   };
-
-   printNewRound();
+   }
 
    return {
-      playRound,
-      getActivePlayer
+      activePlayer,
+      playRound
    };
- }
-
- const game = GameController(player1, player2);
+})();
