1176d78bd86785eb2fed3976b100f3532d178e15
Haley Sorensen <sorensenhaley@gmail.com>
2024-05-02 13:42:40 -0600

    Update game to check for winning solutions or a draw.

--- script.js
+++ script.js
@@ -1,61 +1,124 @@
 //Display Board
- const gameboard = (function () {
-   //Our board will be a 3x3 grid
+ const gameboard = (function gameboard(){
    const board = [];
    const rows = 3;
    const columns = 3;
 
-   for(let i = 0; i < rows; i++){
-      //each row in the board array will be represented by another array
+   for (let i = 0; i < rows; i++) {
       board[i] = [];
-      //the columns in the rows will be represented by -
       for (let j = 0; j < columns; j++){
-         board[i].push('-');
-      }
-   }
+         board[i].push(cell().getValue());
+      };
+   };
 
-   return board;
+   const getBoard = () => board;
+
+   const placeMarker = (row, column, player) => {
+      const availableCell = board[row][column] === '-';
+      if (!availableCell) return;
+      return board[row][column].addMarker(player);
+   };
+
+   return {getBoard, placeMarker};
 })();
 
-//create player objects that hold their name and marker for the game
-function createPlayer (name, marker) {
+function cell() {
+   let value = '-';
+
+   const getValue = () => value;
+
+   const addMarker = (playerMarker) => {
+      return value = `${playerMarker}`;
+   };
+
    return {
-      name,
-      marker
-   }
+      addMarker,
+      getValue
+   };
 };
 
-const player1 = createPlayer('player 1', 'X');
-const player2 = createPlayer('player 2', 'O');
+const gameController = (function gameController(
+   playerOneName = 'Player 1',
+   playerTwoName = 'Player 2'
+){
+   const board = gameboard.getBoard();
 
-//object to play the game
-const game = (function (){
-   const board = gameboard;
-   console.log(board);
-
-   //Keep players in array to easily switch between turns
    const players = [
-      player1,
-      player2
-   ]
+      {name: playerOneName, marker: 'X'},
+      {name: playerTwoName, marker: 'O'}
+   ];
    
    let activePlayer = players[0];
+
    const switchPlayerTurn = () => {
       activePlayer = activePlayer === players[0] ? players[1] : players[0];
-   }
+   };
 
    const getActivePlayer = () => activePlayer;
 
-   console.log(activePlayer);
-   //Rounds
+   let gameWon = false;
+
+   const checkWinner = () => {
+      const [
+         [cell1, cell2, cell3],
+         [cell4, cell5, cell6],
+         [cell7, cell8, cell9]
+      ] = board;
+
+      const winningCombinations = [
+         [cell1, cell2, cell3], //row combinations
+         [cell4, cell5, cell6],
+         [cell7, cell8, cell9],
+         [cell1, cell4, cell7], //column combinations
+         [cell2, cell5, cell8],
+         [cell3, cell6, cell9],
+         [cell1, cell5, cell9], //diagonal combinations
+         [cell3, cell5, cell7]
+      ]
+
+      for (let w = 0; w < winningCombinations.length; w++){
+         const winCombination = winningCombinations[w];
+         const cellA = winCombination[0];
+         const cellB = winCombination[1];
+         const cellC = winCombination[2];
+
+      if (cellA !== '-' && cellB !== '-' && cellC !== '-'){
+         if (cellA === cellB && cellB === cellC){
+            console.log(`${getActivePlayer().name} won.`);
+            gameWon = true;
+         };
+      }
+      };
+   };
+
+   const checkForDraw = () => {
+      let availableCells = 0;
+
+      for (let r = 0; r < board.length; r++){
+         for (let c = 0; c < board[r].length; c++){
+            board[r][c] === '-'
+            ? availableCells++ : (availableCells = availableCells);
+         };
+      };
+
+      if (availableCells === 0 && gameWon === false){
+         console.log('it is a draw.');
+      };
+
+      return (console.log(availableCells));
+   };
+
    const playRound = (row, column) => {
       board[row][column] = getActivePlayer().marker;
-      console.log(board);
+      checkForDraw();
+      checkWinner();
       switchPlayerTurn();
-   }
+      return board;
+   };
 
    return {
       activePlayer,
+      getActivePlayer,
       playRound
    };
 })();
